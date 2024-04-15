861070b41c0548784bd143fd422384d1788a70c5
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-04-15 19:59:26 +0100

    Add RTOS task for dac outupt testing

--- src/stm32g4_main.c
+++ src/stm32g4_main.c
@@ -1,5 +1,7 @@
 #include "stm32g4_main.h"
 #include "stm32g4xx_audio.h"
+#include "stm32g4xx_timers.h"
+#include "dac.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
@@ -18,18 +20,24 @@ void vApplicationStackOverflowHook(xTaskHandle pxTask, char *pcTaskName)
 }
 
 
-static void task1(void *args __attribute((unused)))
+static void dac_task(void *args __attribute((unused)))
 {
+	// Output to speaker
+	unsigned int frequency = 1000; // Hz
+	tim2_interrupt_frequency(frequency);
+	struct DacTxRegisters dac_tx = { &DAC1->DHR8R1, &DAC1->DHR12R1, &DAC1->DHR12L1 };
+	trigger_dac(dac_tx, 0x00, TwelveBitLeft); // base value of triangle wave
 	for (;;) {
-		vTaskDelay(pdMS_TO_TICKS(100));
+		// Task (timer is handling dac updated, nothing to do)
 	}
 }
 
 int main (void)
 {
+	timer_setup(1);
 	setup_hw_dac();
 
-	xTaskCreate(task1, "Flash_LED", 100, NULL, configMAX_PRIORITIES-1, NULL);
+	xTaskCreate(dac_task, "DAC out", 100, NULL, configMAX_PRIORITIES-1, NULL);
 	vTaskStartScheduler();
 
 	for (;;) {
