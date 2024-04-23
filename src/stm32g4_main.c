58365307336dd8ce2986447589cbba57a4af71af
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-04-23 23:05:26 +0100

    Initial led matrix test
    - All lights on matrix light up

--- src/stm32g4_main.c
+++ src/stm32g4_main.c
@@ -1,12 +1,29 @@
 #include "stm32g4_main.h"
+#include "stm32g4xx_spi.h"
 #include "stm32g4xx_audio.h"
 #include "stm32g4xx_timers.h"
 #include "dac.h"
+#include "spi.h"
+#include "led_matrix.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
 #include "stm32g4xx_hal.h"
 #include "stm32g4xx_nucleo.h"
+
+static void led_matrix_test(void)
+{
+	struct LedSpiPin led_cs = { &GPIOA->BSRR, &GPIOA->ODR, SPI_CS_PIN };
+	struct MaximMax2719 led_matrix;
+	set_led_cs_pin_details(&led_matrix.cs, &led_cs);
+
+	led_matrix_transfer_data(led_matrix.cs, &SPI1->DR, 0x02, ADDR_INTENSITY);
+	led_matrix_transfer_data(led_matrix.cs, &SPI1->DR, DATA_SHUTDOWN_OFF, ADDR_SHUTDOWN);
+	led_matrix_transfer_data(led_matrix.cs, &SPI1->DR, DATA_DISPTEST_ON, ADDR_DISPTEST);
+
+	for (;;) {
+	}
+}
 
 // Must define when using a non-zero config for stack overflow
 void vApplicationStackOverflowHook(xTaskHandle pxTask, char *pcTaskName)
@@ -36,6 +53,9 @@ int main (void)
 {
 	timer_setup(1);
 	setup_hw_dac();
+	setup_hw_spi();
+
+	led_matrix_test();
 
 	BaseType_t ret_val = xTaskCreate(dac_task, "DAC out", 100, NULL, configMAX_PRIORITIES-1, NULL);
 	(void) ret_val; // suppress compiler warning
