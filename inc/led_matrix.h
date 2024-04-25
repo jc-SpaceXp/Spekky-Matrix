a8ed7cc0ded921a80a5019098b3a96b9b1285c69
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-04-25 15:40:18 +0100

    Migrate led matrix constants into their own header file
    - Added some additional/alternative names too

--- inc/led_matrix.h
+++ inc/led_matrix.h
@@ -2,41 +2,7 @@
 #define LED_MATRIX_H
 
 #include "led_matrix_private.h"
-
-// Address mappings
-#define ADDR_NOP       0x00U
-#define ADDR_DIGIT0    0x01U
-#define ADDR_DIGIT1    0x02U
-#define ADDR_DIGIT2    0x03U
-#define ADDR_DIGIT3    0x04U
-#define ADDR_DIGIT4    0x05U
-#define ADDR_DIGIT5    0x06U
-#define ADDR_DIGIT6    0x07U
-#define ADDR_DIGIT7    0x08U
-#define ADDR_DECODE    0x09U
-#define ADDR_INTENSITY 0x0AU
-#define ADDR_SCANLIMIT 0x0BU
-#define ADDR_SHUTDOWN  0x0CU
-#define ADDR_DISPTEST  0x0FU
-// Data constants
-#define DATA_SHUTDOWN_ON   0x00U
-#define DATA_SHUTDOWN_OFF  0x01U
-// Decode, set uses binary format e.g. 0b111 will set digit 7
-// otherwise each bit refers to a different segment/line
-#define DATA_DECODE_NONE        0x00U
-#define DATA_DECODE_ALL_DIGITS  0xFFU
-#define DATA_DECODE_SET_DIGITS(x) (x) // set bits D7-D0 to set BCD for those
-// alternative name would be UPTO0, UPTO1 etc.
-#define DATA_SCANLIMIT_0         0x00U
-#define DATA_SCANLIMIT_01        0x01U
-#define DATA_SCANLIMIT_012       0x02U
-#define DATA_SCANLIMIT_0123      0x03U
-#define DATA_SCANLIMIT_01234     0x04U
-#define DATA_SCANLIMIT_012345    0x05U
-#define DATA_SCANLIMIT_0123456   0x06U
-#define DATA_SCANLIMIT_01234567  0x07U
-#define DATA_DISPTEST_ON    0x01U
-#define DATA_DISPTEST_OFF   0x00U
+#include "led_matrix_constants.h"
 
 
 void set_spi_pin_details(struct LedSpiPin* spi_pin
---
+++ inc/led_matrix_constants.h
@@ -0,0 +1,60 @@
+#ifndef LED_MATRIX_CONSTANTS_H
+#define LED_MATRIX_CONSTANTS_H
+
+// Address mappings
+#define ADDR_NOP       0x00U
+#define ADDR_ROW0      0x01U
+#define ADDR_ROW1      0x02U
+#define ADDR_ROW2      0x03U
+#define ADDR_ROW3      0x04U
+#define ADDR_ROW4      0x05U
+#define ADDR_ROW5      0x06U
+#define ADDR_ROW6      0x07U
+#define ADDR_ROW7      0x08U
+#define ADDR_DECODE    0x09U
+#define ADDR_INTENSITY 0x0AU
+#define ADDR_SCANLIMIT 0x0BU
+#define ADDR_SHUTDOWN  0x0CU
+#define ADDR_DISPTEST  0x0FU
+// Alternative namses: 7-segment names
+#define ADDR_DIGIT0    ADDR_ROW0
+#define ADDR_DIGIT1    ADDR_ROW1
+#define ADDR_DIGIT2    ADDR_ROW2
+#define ADDR_DIGIT3    ADDR_ROW3
+#define ADDR_DIGIT4    ADDR_ROW4
+#define ADDR_DIGIT5    ADDR_ROW5
+#define ADDR_DIGIT6    ADDR_ROW6
+#define ADDR_DIGIT7    ADDR_ROW7
+
+// Data constants
+#define DATA_SHUTDOWN_ON   0x00U
+#define DATA_SHUTDOWN_OFF  0x01U
+// Decode, set uses binary format e.g. 0b111 will set digit 7 for 7-segment display
+// otherwise each bit refers to a different segment/line
+#define DATA_DECODE_NONE           0x00U
+#define DATA_DECODE_ALL_ROWS       0xFFU
+#define DATA_DECODE_SET_ROWS(x)    (x) // set bits D7-D0 to set BCD for those
+#define DATA_SCANLIMIT_0_ROWS_MAX    0x00U
+#define DATA_SCANLIMIT_1_ROWS_MAX    0x01U
+#define DATA_SCANLIMIT_2_ROWS_MAX    0x02U
+#define DATA_SCANLIMIT_3_ROWS_MAX    0x03U
+#define DATA_SCANLIMIT_4_ROWS_MAX    0x04U
+#define DATA_SCANLIMIT_5_ROWS_MAX    0x05U
+#define DATA_SCANLIMIT_6_ROWS_MAX    0x06U
+#define DATA_SCANLIMIT_7_ROWS_MAX    0x07U
+#define DATA_DISPTEST_ON           0x01U
+#define DATA_DISPTEST_OFF          0x00U
+// Alternative names
+#define DATA_DECODE_ALL_DIGITS     DATA_DECODE_ALL_ROWS
+#define DATA_DECODE_SET_DIGITS(x)  DATA_DECODE_SET_ROWS(x)
+#define DATA_SCANLIMIT_1_ROW_MAX   DATA_SCANLIMIT_1_ROWS_MAX
+#define DATA_SCANLIMIT_0_ROWS      DATA_SCANLIMIT_0_ROWS_MAX
+#define DATA_SCANLIMIT_1_ROWS      DATA_SCANLIMIT_1_ROWS_MAX
+#define DATA_SCANLIMIT_2_ROWS      DATA_SCANLIMIT_2_ROWS_MAX
+#define DATA_SCANLIMIT_3_ROWS      DATA_SCANLIMIT_3_ROWS_MAX
+#define DATA_SCANLIMIT_4_ROWS      DATA_SCANLIMIT_4_ROWS_MAX
+#define DATA_SCANLIMIT_5_ROWS      DATA_SCANLIMIT_5_ROWS_MAX
+#define DATA_SCANLIMIT_6_ROWS      DATA_SCANLIMIT_6_ROWS_MAX
+#define DATA_SCANLIMIT_7_ROWS      DATA_SCANLIMIT_7_ROWS_MAX
+
+#endif /* LED_MATRIX_CONSTANTS_H */
