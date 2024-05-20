4ed1fe0735060d0f464a01a92ee58a4f8da404d9
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-05-20 18:01:01 +0100

    Migrate fft code into rtos subdirectory
    - There will be some sort of FFT task therefore I migrated the current
      code into the rtos subdirectory
    - Can use a one shot timer or vTaskSuspend()/vTaskDelete() to allow
      the scheduler to handle the FFT code

---
+++ inc/rtos/fft_rtos.h
@@ -0,0 +1,9 @@
+#ifndef FFT_RTOS_H
+#define FFT_RTOS_H
+
+#include "FreeRTOS.h"
+#include "timers.h"
+
+void fake_fft_task(void);
+
+#endif /* FFT_RTOS_H */
--- src/spekky_matrix.c
+++ src/spekky_matrix.c
@@ -10,11 +10,9 @@
 #include "task.h"
 #include "timers.h"
 #include "led_matrix_rtos.h"
+#include "fft_rtos.h"
 #include "stm32g4xx_hal.h"
 #include "stm32g4xx_nucleo.h"
-#include "transform_functions.h"
-#include "statistics_functions.h"
-#include "hw_verification/py_sine_125hz_input_test.h"
 
 
 static void dac_task(void *args __attribute((unused)))
@@ -36,23 +34,7 @@ int main (void)
 	led_matrix_setup();
 
 	// Run FFT before RTOS scheduler
-	uint8_t inverse_fft = 0;
-	uint8_t bit_reverse = 1;
-	arm_cfft_instance_f32 arm_cfft;
-	arm_status c_status = arm_cfft_init_1024_f32(&arm_cfft);
-	(void) c_status; // suppress compiler warning
-	assert_param(c_status == ARM_MATH_SUCCESS);
-
-	arm_cfft_f32(&arm_cfft, sine32c_125hz_2048, inverse_fft, bit_reverse);
-	float32_t bin_mags[1024];
-	arm_cmplx_mag_f32(sine32c_125hz_2048, bin_mags, 1024);
-
-	struct FftBinPeak {
-		float32_t magnitude;
-		uint32_t bin_index;
-	} fft_bin_peak = { 0, 0 };
-	arm_max_f32(bin_mags, 1024, &fft_bin_peak.magnitude, &fft_bin_peak.bin_index);
-
+	fake_fft_task();
 
 	BaseType_t ret_val = xTaskCreate(dac_task, "DAC out", 100, NULL, configMAX_PRIORITIES-2, NULL);
 	(void) ret_val; // suppress compiler warning
