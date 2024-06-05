7c6a98faaea579a9412e5020672e81950de3d189
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-06-05 15:01:41 +0100

    Added STM32 I2S setup for SPI2 module
    - Faced some issues here with my device, looking at the NucleoG431KB
      schematic, PF0 and PF1 (WS and CLK) are both unconnected. Solder
      bridges SB8 and SB11 need to be soldered in order to get an output on
      those pins. Verfied that both WS and CLK were working on my logic
      analyser
    - My I2S MEMS microphone also has a SEL pin which selects which channel
      the mic will output to, this will be tied to GND for the left channel
      so no pin assigment was made
    - Added AFH register macro as A11 falls into the alternate function
      register high output
    - Setup I2S to sample at a frequency of 44.1kHz, when using the HSI @
      16MHz

--- inc/mcu/reg_macros.h
+++ inc/mcu/reg_macros.h
@@ -5,10 +5,12 @@
 #define GET_REG_BIT0(REG, PIN) (REG ## PIN ## _0)
 #define GET_REG_BIT1(REG, PIN) (REG ## PIN ## _1)
 #define GET_AFRL_REG(REG, PIN) (REG ## PIN ## _Pos)
+#define GET_AFRH_REG(REG, PIN) (REG ## PIN ## _Pos)
 // Use eGET to expand the PIN macro for GET macros above
 #define eGET_REG(REG, PIN) GET_REG(REG, PIN)
 #define eGET_REG_BIT0(REG, PIN) GET_REG_BIT0(REG, PIN)
 #define eGET_REG_BIT1(REG, PIN) GET_REG_BIT1(REG, PIN)
 #define eGET_AFRL_REG(REG, PIN) GET_AFRL_REG(REG, PIN)
+#define eGET_AFRH_REG(REG, PIN) GET_AFRH_REG(REG, PIN)
 
 #endif /* REG_MACROS_H */
---
+++ src/mcu/stm32g4xx_i2s.c
@@ -0,0 +1,61 @@
+#include "stm32g4xx_i2s.h"
+#include "reg_macros.h"
+
+#include "stm32g4xx.h"
+#include "stm32g4xx_ll_spi.h"  // contains i2s macros
+#include "stm32g4xx_hal_gpio.h"
+#include "stm32g4xx_hal_rcc_ex.h"
+
+
+static void i2s_gpio_setup(void)
+{
+	RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN) | (RCC_AHB2ENR_GPIOFEN);
+	// Set all to inputs
+	GPIOF->MODER &= ~ (eGET_REG(GPIO_MODER_MODE, I2S_CLK_PIN)
+	                  | eGET_REG(GPIO_MODER_MODE, I2S_WS_PIN));
+	GPIOA->MODER &= ~ (eGET_REG(GPIO_MODER_MODE, I2S_SD_PIN));
+	                  //| eGET_REG(GPIO_MODER_MODE, I2S_SEL_PIN));
+	// Set GPIO (and I2S SEL) to outputs and SPI2 to AF
+	GPIOF->MODER |= eGET_REG_BIT1(GPIO_MODER_MODE, I2S_CLK_PIN)
+	                | eGET_REG_BIT1(GPIO_MODER_MODE, I2S_WS_PIN);
+	// Use SPI alternative function (SPI2 and I2S share same pins/registers)
+	GPIOF->AFR[0] |= (GPIO_AF5_SPI2 << eGET_AFRL_REG(GPIO_AFRL_AFSEL, I2S_CLK_PIN))
+	                 | (GPIO_AF5_SPI2 << eGET_AFRL_REG(GPIO_AFRL_AFSEL, I2S_WS_PIN));
+	GPIOA->AFR[1] |= (GPIO_AF5_SPI2 << eGET_AFRH_REG(GPIO_AFRH_AFSEL, I2S_SD_PIN));
+	// All GPIO ports/pins are push-pull by default (no need for OTYPER)
+	// High speed pins
+	GPIOF->OSPEEDR |= eGET_REG_BIT1(GPIO_OSPEEDR_OSPEED, I2S_CLK_PIN)
+	                  | eGET_REG_BIT1(GPIO_OSPEEDR_OSPEED, I2S_WS_PIN);
+	GPIOA->OSPEEDR |= eGET_REG_BIT1(GPIO_OSPEEDR_OSPEED, I2S_SD_PIN);
+}
+
+static void enable_i2s(void)
+{
+	SPI2->I2SCFGR |= SPI_I2SCFGR_I2SE;
+}
+
+void setup_hw_i2s(void)
+{
+	RCC->CCIPR |= RCC_I2SCLKSOURCE_HSI;
+	RCC->APB1ENR1 |= RCC_APB1ENR1_SPI2EN;
+	i2s_gpio_setup();
+
+	// Deafults: (which don't need changing)
+	// 16-bit data
+	// CPHA 0 CPOL 0
+	// Phillips I2S mode
+	// MSB first
+
+	// Setting clock rate to be as close to 44.1 kHz as possible
+	// Fs = Fi2sclk/(32(1)((2*I2SDIV)+ODD) (for no MCLK I2S setting)
+	// Where Fi2sclk is equal to the APB clock for the SPI/I2S block
+	// which is HCLK * APB prescaler
+	// 32((2*I2SDIV)+ODD) = 16E6/44.1E3 --> 2*I2SDIV+ODD = 0.5E6/44.1E3
+	// for 16MHz, ODD = 1 and I2SDIV = 5 are the closest values possible
+	SPI2->I2SPR |= SPI_I2SPR_ODD | 0x05;
+	SPI2->I2SCFGR |= SPI_I2SCFGR_I2SMOD; // I2S mode
+	SPI2->I2SCFGR |= LL_I2S_MODE_MASTER_RX; // STM32 is master and receiving
+
+	// Enable I2S module once setup is complete
+	enable_i2s();
+}
