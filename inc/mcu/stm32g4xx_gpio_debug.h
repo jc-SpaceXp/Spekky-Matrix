96a194d2cada9d0911e2b3e265cd2c0dd9ec4013
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-09-16 15:35:36 +0100

    Add functions for toggling gpio debug pin

--- inc/mcu/stm32g4xx_gpio_debug.h
+++ inc/mcu/stm32g4xx_gpio_debug.h
@@ -2,10 +2,13 @@
 #define STM32G4xx_GPIO_DEBUG_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #define GPIO_DEBUG_PIN   4 // unused in led matrix
 #define GPIO_DEBUG_PORT   GPIOB
 
 void setup_hw_gpio_debug(void);
+void assert_gpio_debug_pin(void);
+void deassert_gpio_debug_pin(void);
 
 #endif /* STM32G4xx_GPIO_DEBUG_H */
--- src/mcu/stm32g4xx_gpio_debug.c
+++ src/mcu/stm32g4xx_gpio_debug.c
@@ -5,7 +5,7 @@
 #include "stm32g4xx_hal_gpio.h"
 
 
-void setup_hw_gpio(void)
+void setup_hw_gpio_debug(void)
 {
 	RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN) | (RCC_AHB2ENR_GPIOBEN);
 	// Set all to inputs
@@ -17,4 +17,15 @@ void setup_hw_gpio(void)
 	GPIOB->OSPEEDR |= eGET_REG_BIT1(GPIO_OSPEEDR_OSPEED, GPIO_DEBUG_PIN);
 	// Clear reset bit on B4 (MISO, no pull-up or pull-down)
 	GPIOB->PUPDR &= ~eGET_REG(GPIO_PUPDR_PUPD, GPIO_DEBUG_PIN);
+}
+
+// Favour atomic writes rather than reusing spi assert/deassert pin code
+void assert_gpio_debug_pin(void)
+{
+	GPIO_DEBUG_PORT->BSRR |= (1 << GPIO_DEBUG_PIN);
+}
+
+void deassert_gpio_debug_pin(void)
+{
+	GPIO_DEBUG_PORT->BSRR |= (1 << (GPIO_DEBUG_PIN + 16));
 }
