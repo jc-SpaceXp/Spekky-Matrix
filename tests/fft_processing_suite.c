37bc6813a4bcb52e8953e27a12e3f40ac3562132
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-10-04 14:10:47 +0100

    fft to db fs function now expects a 24-bit magnitude
    - Using a 24-bit magnitude gives us dB FS values we expect for a maximum
      amplitude square wave (0dB FS for 0th harmonic (DC) and ~ -3.8 dB for
      1st harmonic)
    - Added a unit test to verify the 1st harmonic is above -6dB FS
    - Maximum magnitude is now a 24-bit value rather than a 32-bit one, fft
      to dB FS function has made this adjustment in log calculation
    - Adjusted some comments too

--- tests/fft_processing_suite.c
+++ tests/fft_processing_suite.c
@@ -14,11 +14,12 @@ TEST max_amp_square_wave_to_db_fs(void)
 	float dst[32] = { 0 };
 	real_fft_to_db_fs(real_bin_mags_max_square_wave24, dst, 32);
 
+	ASSERT_LT(-6.0f, dst[0]); // 1st harmonic should be above -6 dB FS
 	float prev_odd_harmonic = dst[0];
 	for (int i = 0; i < 16; ++i) {
 		float odd_harmonic = dst[i * 2];
 		float even_harmonic = dst[(i * 2) + 1];
-		ASSERT_GT(-150.0f, even_harmonic); // even harmonics should be below -150 dB
+		ASSERT_GT(-150.0f, even_harmonic); // even harmonics should be below -150 dB FS
 		if (i) {
 			ASSERT_GT(prev_odd_harmonic, odd_harmonic); // prev > new (1st > 3rd etc.)
 		}
