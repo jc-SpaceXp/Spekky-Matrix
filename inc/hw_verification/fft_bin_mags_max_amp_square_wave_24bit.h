9fb47ee99747429cea76a1da0ae7b993fbb82da5
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-10-04 08:05:37 +0100

    Add gdb output of fft (max amp square wave)
    - 1st index is the 1st harmonic as we've skipped the DC component

---
+++ inc/hw_verification/fft_bin_mags_max_amp_square_wave_24bit.h
@@ -0,0 +1,40 @@
+#ifndef FFT_BIN_MAGS_AMP_SQUARE_WAVE_24BIT_H
+#define FFT_BIN_MAGS_AMP_SQUARE_WAVE_24BIT_H
+
+// captured from gdb (1st bin is the non-DC bin, 1st harmonic)
+float real_bin_mags_max_square_wave24[32] = {
+	5217.28418
+	, 0
+	, 1744.69543
+	, 0
+	, 1053.58386
+	, 0
+	, 759.892395
+	, 0
+	, 598.753357
+	, 0
+	, 497.954926
+	, 0
+	, 429.746979
+	, 0
+	, 381.202332
+	, 0
+	, 345.501984
+	, 0
+	, 318.722107
+	, 0
+	, 298.462738
+	, 0
+	, 283.189209
+	, 0
+	, 271.893738
+	, 0
+	, 263.909027
+	, 0
+	, 258.801117
+	, 0
+	, 256.308716
+	, 0
+};
+
+#endif  /* FFT_BIN_MAGS_AMP_SQUARE_WAVE_24BIT_H */
