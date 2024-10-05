dfd6841b54962df7d02f66879d71f2edfcbd5d9a
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-10-05 16:51:55 +0100

    Initial attempt at double buffering for fft averaging
    - A delay to the queue sending function is needed for a similar
      experience to the non double buffered version

--- src/rtos/fft_rtos.c
+++ src/rtos/fft_rtos.c
@@ -16,7 +16,7 @@
 extern QueueHandle_t xDmaFlagQueue;
 extern QueueHandle_t xFftCompleteFlagQueue;
 
-float32_t bin_mags[6][FFT_DATA_SIZE/2];
+float32_t bin_mags[12][FFT_DATA_SIZE/2];
 float32_t average_bin_mags[FFT_DATA_SIZE/2];
 float32_t db_bin_mags[FFT_DATA_SIZE/2];
 static float32_t fft_buffer1[FFT_DATA_SIZE * 2];
@@ -60,12 +60,18 @@ void fft_task_processing(void* pvParameters)
 
 
 		fft_counter += 1;
-		if (fft_counter > 5) {
+		if (fft_counter > 11) {
+			average_bin_2d_array(6, FFT_DATA_SIZE/2, &bin_mags[6], average_bin_mags);
+			real_fft_to_db_fs(average_bin_mags, db_bin_mags, FFT_DATA_SIZE/2);
+
+			xQueueSendToFront(xFftCompleteFlagQueue, &fft_counter, pdMS_TO_TICKS(2));
+			fft_counter = 0;
+		} else if (fft_counter > 5) {
+			// 0-5
 			average_bin_2d_array(6, FFT_DATA_SIZE/2, bin_mags, average_bin_mags);
 			real_fft_to_db_fs(average_bin_mags, db_bin_mags, FFT_DATA_SIZE/2);
 
-			xQueueSendToFront(xFftCompleteFlagQueue, &fft_counter, 0);
-			fft_counter = 0;
+			xQueueSendToFront(xFftCompleteFlagQueue, &fft_counter, pdMS_TO_TICKS(2));
 		}
 	}
 }
