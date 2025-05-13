a50438fc4dd189247ca9c00e2ced0d7d8b6f409b
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2025-05-13 14:37:00 +0100

    Add gpio debug files (from timing branch)
    - Rather than having a separate timing branch, timing pataches will be
      used and kept up to date (so there isn't any divergence from the main
      or any new feature branches)

---
+++ src/mcu/stm32g4xx_gpio_debug.c
@@ -0,0 +1,31 @@
+#include "stm32g4xx_gpio_debug.h"
+#include "reg_macros.h"
+
+#include "stm32g4xx.h"
+#include "stm32g4xx_hal_gpio.h"
+
+
+void setup_hw_gpio_debug(void)
+{
+	RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN) | (RCC_AHB2ENR_GPIOBEN);
+	// Set all to inputs
+	GPIO_DEBUG_PORT->MODER &= ~ eGET_REG(GPIO_MODER_MODE, GPIO_DEBUG_PIN);
+	// Set GPIO to outputs
+	GPIO_DEBUG_PORT->MODER |= eGET_REG_BIT0(GPIO_MODER_MODE, GPIO_DEBUG_PIN);
+	// All GPIO ports/pins are push-pull by default (no need for OTYPER)
+	// High speed pins
+	GPIO_DEBUG_PORT->OSPEEDR |= eGET_REG_BIT1(GPIO_OSPEEDR_OSPEED, GPIO_DEBUG_PIN);
+	// Clear reset bit on B4 (MISO, no pull-up or pull-down)
+	GPIO_DEBUG_PORT->PUPDR &= ~eGET_REG(GPIO_PUPDR_PUPD, GPIO_DEBUG_PIN);
+}
+
+// Favour atomic writes rather than reusing spi assert/deassert pin code
+void assert_gpio_debug_pin(void)
+{
+	GPIO_DEBUG_PORT->BSRR |= (1 << GPIO_DEBUG_PIN);
+}
+
+void deassert_gpio_debug_pin(void)
+{
+	GPIO_DEBUG_PORT->BSRR |= (1 << (GPIO_DEBUG_PIN + 16));
+}
