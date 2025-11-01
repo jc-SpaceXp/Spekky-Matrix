bb6b3729ee37e5a4c267fa3c93f6bfbeb4fa6e00
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2025-11-01 21:29:55 +0000

    Pull oe to gnd
    - OE needs to be pulled to GND if we wish to set any LED (drives the
      outputs of each STP16 led driver IC)

--- src/rtos/led_matrix_rtos.c
+++ src/rtos/led_matrix_rtos.c
@@ -26,6 +26,9 @@ void led_matrix_setup(int total_devices)
 	set_total_led_matrix_devices(&led_matrix, total_devices);
 
 	max7219_led_matrix_init_all_quick(led_matrix, &SPI1->DR, DATA_BRIGHTNESS_LEVEL1);
+
+	// Pull OE to GND (all LEDs could be set)
+	deassert_spi_pin(led_cs.assert_address, SPI_OE_PIN);
 }
 
 void led_matrix_update_task(void* pvParameters)
