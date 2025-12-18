2064466458232dfd6829d579482096cde5be3c2d
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2025-12-18 18:34:30 +0000

    Fix led ghosting
    - Only my previous bar pattern code didn't show any ghosting on the LEDs
    - Changing the pattern of the inversions caused ghosting
    - Fixed ghosting by blanking all LEDs on the row before switching to the
      next row
    - Adjacent row only has very minor ghosting for one LED (for this
      pattern)
    - Tested on hardware and works perfectly (all LEDs the same brightness)
    - Also flipped the LED matrix vertically and horizontally, essentially
      position 101 is the bottom left rather than the top right (for the
      PCB silkscreen)

--- src/rtos/led_matrix_rtos.c
+++ src/rtos/led_matrix_rtos.c
@@ -27,7 +27,7 @@ void led_matrix_setup(int total_devices)
 	copy_spi_pin_details(&led_matrix.oe, &led_oe);
 	set_total_stp16cp05_led_matrix_devices(&led_matrix, total_devices);
 
-	// Pull OE to GND (all LEDs could be set)
+	// Pull OE to GND (all LEDs could be set, active low)
 	deassert_spi_pin(led_oe.assert_address, led_oe.pin);
 }
 
@@ -51,18 +51,36 @@ void led_matrix_update_task(void* pvParameters)
 
 		for (int i = 0; i < COLS; ++i) {
 			//bars[i] = fft_to_led_bar_conversion(db_bin_mags[i]);
-			bars[i] = i / 4;
+			bars[i] = (i % 8) + 1;
 		}
+		bars[0] = 0; // keep 1st col empty to display an empty column
 
 		led_matrix_bar_conversion_32bit(&bars[0], COLS, ROWS, Vertical, row_outputs);
+		led_matrix_inversions_32bit(row_outputs, ROWS, DoFlipLeftRight, DoFlipVertically);
 
 		for (int i = 0; i < ROWS; ++i) {
 			set_led_matrix_device_cascade_bytes(tx_data, 0
-			                                           , led_matrix_set_bit_in_row_conversion(i));
+			                                   , led_matrix_set_bit_in_row_conversion(i));
 			set_led_matrix_device_cascade_bytes(tx_data, 1, (uint16_t) row_outputs[i]);
 			set_led_matrix_device_cascade_bytes(tx_data, 2, (uint16_t) (row_outputs[i] >> 16u));
 			generic_led_matrix_transfer_data_cascade(led_matrix.le, &SPI1->DR, tx_data
 			                                        , led_matrix.total_devices, NormalCascade);
+
+			set_led_matrix_device_cascade_bytes(tx_data, 0
+			                                   , led_matrix_set_bit_in_row_conversion(i));
+			// BLANK TWICE TO REDUCE GHOSTING
+			// blank led to turn off transistors before switching rows
+			tx_data[1] = 0x0000;
+			tx_data[2] = 0x0000;
+			generic_led_matrix_transfer_data_cascade(led_matrix.le, &SPI1->DR, tx_data
+			                                        , led_matrix.total_devices, NormalCascade);
+
+			// blank led to turn off transistors before switching rows
+			set_led_matrix_device_cascade_bytes(tx_data, 0
+			                                   , led_matrix_set_bit_in_row_conversion(i));
+			generic_led_matrix_transfer_data_cascade(led_matrix.le, &SPI1->DR, tx_data
+			                                        , led_matrix.total_devices, NormalCascade);
 		}
+
 	}
 }
